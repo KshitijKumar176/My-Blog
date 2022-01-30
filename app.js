3d9feab99593b94741cfe60c2519ec8144354df8
Kshitij Kumar <physiomatica@gmail.com>
2022-01-30 13:49:53 +0530

    port change

--- app.js
+++ app.js
@@ -19,11 +19,14 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  "mongodb+srv://admin-kshitij:Test123@cluster0.1pf3h.mongodb.net/blogDB?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+);
 
 const postSchema = {
   title: String,
@@ -131,6 +134,11 @@ app.get("/post/delete/:id", function (req, res) {
   });
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("Server has started successfully");
 });
